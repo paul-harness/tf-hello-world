45448933214064a7b644deb33b0d21f9254ae8fe
paul-harness <56152762+paul-harness@users.noreply.github.com>
2022-05-05 06:10:09 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
-output "hello_world" {	
-	value = "Hello, World!"
+#use terraform apply -var="aws_region=us.east1" -auto-approve
+variable "aws_region" {
+  type        = string
+  description = "The AWS region in which resources will be created"
+}
+
+output "aws_region" {	
+	value = var.aws_region
 }
