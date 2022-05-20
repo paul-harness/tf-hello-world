d4cb437531ccac92d389e125428327b425b30bca
paul-harness <56152762+paul-harness@users.noreply.github.com>
2022-05-20 05:31:57 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,27 @@ variable "aws_region" {
 output "aws_region" {	
 	value = var.aws_region
 }
+variable "var1" {
+  type        = string
+  description = "The first variable"
+}
+
+output "aws_region" {	
+	value = var.var1
+}
+variable "var2" {
+  type        = string
+  description = "The second variable"
+}
+
+output "aws_region" {	
+	value = var.var2
+}
+variable "var3" {
+  type        = string
+  description = "The third variable"
+}
+
+output "aws_region" {	
+	value = var.var3
+}
